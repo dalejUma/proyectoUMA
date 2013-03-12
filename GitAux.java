604f9ec2103806c3a33b7f327f84ec32b6e8118f
Daniel Alejandro Castro García <d.alejandro91@alu.uma.es>
2013-03-12 10:56:51 +0100

    Primera subida

---
+++ GitAux.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgitejemplo;
+
+/**
+ *
+ * @author Daniel Alejandro Castro García
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+    
+    public void metodoComunitario(){
+        //Línea nueva de comentario
+        System.out.println("Aqui escribimos todos");
+    }
+    
+    public void testGitAux(){
+        //Método branch testing
+        System.out.println("Método testing");
+    }
+}
